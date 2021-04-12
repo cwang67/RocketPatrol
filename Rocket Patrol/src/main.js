5f273fe7509a38c3ff54586fb2d8575504e72211
Carey Wang <cwang228@ucsc.edu>
2021-04-11 22:41:47 -0700

    Loading rocket

--- Rocket Patrol/src/main.js
+++ Rocket Patrol/src/main.js
@@ -10,5 +10,3 @@ let game = new Phaser.Game(config);
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 
-//reserve keyboard variables
-let keyF, keyR, keyLEFT, keyRIGHT;
