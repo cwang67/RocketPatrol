9df29c1c35e472ebac1c8addac45ff52cbc2c0d7
Carey Wang <cwang228@ucsc.edu>
2021-04-12 02:38:35 -0700

    added menu text

--- Rocket Patrol/src/scenes/Menu.js
+++ Rocket Patrol/src/scenes/Menu.js
@@ -26,5 +26,15 @@ create()
         },
         fixedWidth: 0
     }
+      // show menu text
+      this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
+      this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
+      menuConfig.backgroundColor = '#00FF00';
+      menuConfig.color = '#000';
+      this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
+
+      // define keys
+      keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+      keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 }
 }
