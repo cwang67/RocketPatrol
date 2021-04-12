fa8d70eaf3ec3c51db7e915913615e4435cc161c
Carey Wang <cwang228@ucsc.edu>
2021-04-12 01:42:12 -0700

    preload audio

--- Rocket Patrol/src/scenes/Menu.js
+++ Rocket Patrol/src/scenes/Menu.js
@@ -5,10 +5,10 @@ class Menu extends Phaser.Scene {
 
 preload()
 {
-    //loads audio
-    this.load.audio('sfx_select', './assets/blip_select12.wav');
-    this.load.audio('sfx_explosion', './assets/explosion38.wav');
-    this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+    // load audio
+  this.load.audio('sfx_select', './assets/blip_select12.wav');
+  this.load.audio('sfx_explosion', './assets/explosion38.wav');
+  this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
 }
 
 
@@ -17,4 +17,5 @@ create()
     this.add.text(20, 20, "Rocket Patrol Menu");
     this.scene.start("playScene");
 }
+
 }
