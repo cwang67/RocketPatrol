958f87245813a899403631088311595562409900
Carey Wang <cwang228@ucsc.edu>
2021-04-11 22:55:08 -0700

    rocket

--- Rocket Patrol/src/prefabs/Rocket.js
+++ Rocket Patrol/src/prefabs/Rocket.js
@@ -1,6 +1,7 @@
 // Rocket prefab
 class Rocket extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, texture, frame) {
+    constructor(scene, x, y, texture, frame) 
+    {
       super(scene, x, y, texture, frame);
   
       // add object to existing scene
