e7660f789a501fbeee9ed05fe6303131b1c8c3a8
Carey Wang <cwang228@ucsc.edu>
2021-04-11 21:46:03 -0700

    Rocket Movement

--- Rocket Patrol/src/scenes/Play.js
+++ Rocket Patrol/src/scenes/Play.js
@@ -29,11 +29,18 @@ create()
 
     // add rocket (p1)
     this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize, 'rocket').setOrigin(0.5, 0);
+
+    //define keyboard variables
+    keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+    keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+    keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+    keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 }
 
 update()
 {
     this.starfield.tilePositionX -= 4;
+    this.p1Rocket,update();
 }
 
 }
