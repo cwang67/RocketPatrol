b422d65f90671f31672ea7160c860a52cb562701
Carey Wang <cwang228@ucsc.edu>
2021-04-11 23:25:41 -0700

    collision check

--- Rocket Patrol/src/prefabs/Spaceship.js
+++ Rocket Patrol/src/prefabs/Spaceship.js
@@ -25,4 +25,10 @@ class Spaceship extends Phaser.GameObjects.Sprite
             this.x = game.config.width;
         }
     }
+
+    //position reset
+    reset()
+    {
+        this.x = game.config.width;
+    }
 }
