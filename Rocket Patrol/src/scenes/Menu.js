282beb5f1c63d4132be0fb18d7aab30fd0e800c9
Carey Wang <cwang228@ucsc.edu>
2021-04-12 02:37:53 -0700

    menu screen

--- Rocket Patrol/src/scenes/Menu.js
+++ Rocket Patrol/src/scenes/Menu.js
@@ -12,8 +12,19 @@ class Menu extends Phaser.Scene {
 
 create()
 {
-    this.add.text(20, 20, "Rocket Patrol Menu");
-    this.scene.start("playScene");
+    // menu text configuration
+    let menuConfig = 
+    {
+        fontFamily: 'Courier',
+        fontSize: '28px',
+        backgroundColor: '#F3B141',
+        color: '#843605',
+        align: 'right',
+        padding: {
+            top: 5,
+            bottom: 5,
+        },
+        fixedWidth: 0
+    }
 }
-
 }
