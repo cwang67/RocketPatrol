99432d09e41af20eee44b927a477698062b458e6
Carey Wang <cwang228@ucsc.edu>
2021-04-18 21:17:41 -0700

    changed asset names

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,9 +6,9 @@ class Play extends Phaser.Scene {
 preload()
 {
     //loads images/tile sprites
-    this.load.image('rocket', './assets/rocket.png');
-    this.load.image('spaceship', './assets/spaceship.png');
-    this.load.image('starfield', './assets/starfield.png');
+    this.load.image('banana', './assets/banana.png');
+    this.load.image('monkey', './assets/monkey.png');
+    this.load.image('forest', './assets/forest.png');
     //load spritesheet
     this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 0});
 }
@@ -18,7 +18,7 @@ preload()
 create()
 {
     //place tile sprite
-    this.starfield = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'starfield').setOrigin(0,0);
+    this.starfield = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'forest').setOrigin(0,0);
 
     //green UI Background
     this.add.rectangle(0,borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0,0);
@@ -30,12 +30,12 @@ create()
     this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
 
     // add rocket (p1)
-    this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
+    this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'banana').setOrigin(0.5, 0);
 
     // add spaceships (x3)
-    this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
-    this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
-    this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
+    this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'monkey', 0, 30).setOrigin(0, 0);
+    this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'monkey', 0, 20).setOrigin(0,0);
+    this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'monkey', 0, 10).setOrigin(0,0);
 
     // define keys
     keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
