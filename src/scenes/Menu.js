132a05449982138188b00b22111bc79890b6b460
Carey Wang <cwang228@ucsc.edu>
2021-04-19 04:53:58 -0700

    edit

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -37,9 +37,7 @@ create()
       keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
       keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 
-      // define keys
-    keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-    keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+  
 }
 
 update() {
