454bfcf356b33ffdcc509fc230eb1cfdd2ed78f1
Carey Wang <cwang228@ucsc.edu>
2021-04-21 07:27:23 -0700

    added requirements to source code

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
+// Carey Wang
+// Modded Rocket Patrol
+// 04/21/2021
+// 20 hours to complete
+// Implement a simultaneous two-player mode (30)
+// Display the time remaining (in seconds) on the screen (10)
+// Track a high score that persists across scenes and display it in the UI (5)
+// Add your own (copyright-free) background music to the Play scene (5)
+// Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)
+// Use Phaser's particle emitter to create a particle explosion when the rocket hits the spaceship (20)
+// Create 4 new explosion SFX and randomize which one plays on impact (10)
+// Received help from https://github.com/Ghosin9/Rocket-Patrol and https://github.com/rsmitchell179/Rocket-Patrol-With-Mods
+
+
+
+
+
 let config = {
   type: Phaser.CANVAS,
   width: 640,
